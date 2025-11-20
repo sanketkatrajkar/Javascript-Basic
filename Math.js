36ae79dc67f52b122ea7cb276ea29fac2ecd02c4
sanketkatrajkar <135363208+sanketkatrajkar@users.noreply.github.com>
2025-11-20 14:30:46 +0530

    Math Object

---
+++ Math.js
@@ -0,0 +1,17 @@
+Math.abs(12);   //112
+Math.abs(-12);  // 12
+Math.pow(2,3)   // 8
+Math.floor(5.5) // 5 rountoff number >= nearest smallest integers values 
+Math.floor(5.9999999999)  //
+Math.floor(-5.5) // -6
+Math.ceil(5.5) // 6 roundoff number <= lasgest number
+Math.ceil(5.000001) // 6
+Math.random() // Konta pn number deil vegvegali 
+
+
+let num = Math.random();
+
+console.log(num);
+num = num * 10;
+
+console.log(num);
