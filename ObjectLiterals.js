b579983db11e3d884cde0add1f8687def3ec24c1
sanketkatrajkar <135363208+sanketkatrajkar@users.noreply.github.com>
2025-11-20 14:31:41 +0530

    Object Literals

---
+++ ObjectLiterals.js
@@ -0,0 +1,51 @@
+let student = {
+    name : "Sanket",
+    age : 22,
+    marks : 88
+};
+
+console.log(student);
+
+// Get values from Object
+console.log(student["name"]);
+console.log(student.age);
+
+
+const obj = {
+    1: "a",
+    2: "b",
+    true: "c",
+    null: "d",
+    undefined: "e"
+};
+
+
+console.log(obj);
+console.log(obj.null = "z");
+console.log(obj);
+console.log(obj[1]);
+
+const classinfo = [
+
+    {
+        name: "Sanket",
+        age: 22,
+        city : "Pune"
+    },
+
+    {
+        name: "aniket",
+        age: 23,
+        city : "Nashik"
+    },
+
+    {
+        name: "ram",
+        age: 25,
+        city : "nagar"
+    },
+];
+
+console.log(classinfo);
+console.log(classinfo[0].name);
+
